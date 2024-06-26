f6f5d2782c3f855398bf6701de43717509538366
Sophia Xie <xiesophia7@gmail.com>
2024-06-26 09:07:20 -0400

    give mpnn script cmd line args

--- scripts/proteinmpnn.sh
+++ scripts/proteinmpnn.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+### Script to run ProteinMPNN.
+
 set -e
 
 repo_dir=/hpf/tools/alma8/ProteinMPNN/v1.0.1/
@@ -8,11 +10,24 @@ echo Loading module...
 
 module load ProteinMPNN/v1.0.1
 
-name="1lvm_top70"
-input_pdb=~/proteinmpnn-redesign/inputs/1lvm.pdb
-chains_to_design="A"
-fixed_positions=~/proteinmpnn-redesign/inputs/top70.jsonl
-output_dir=~/proteinmpnn-redesign/outputs/1lvm/
+### REQUIRED PARAMETERS
+# 1: input pdb (MUST have a unique name, ProteinMPNN uses this as its output file name)
+# 2: chains to design (e.g. "A")
+# 3: fixed positions .jsonl
+# 4: num sequences to design per temperature per target
+# 5: output dir
+
+input_pdb=$1
+chains_to_design=$2
+fixed_positions=$3
+seq_per_target=$4
+output_dir=$5
+
+echo INPUT PDB $input_pdb
+echo CHAINS TO DESIGN $chains_to_design
+echo FIXED POSITIONS $fixed_positions
+echo SEQ PER TARGET $seq_per_target
+echo OUTPUT DIR $output_dir
 
 echo Running script...
 
@@ -21,14 +36,8 @@ python $repo_dir/protein_mpnn_run.py \
 	--pdb_path_chains $chains_to_design \
 	--fixed_positions_jsonl $fixed_positions \
 	--out_folder $output_dir \
-	--num_seq_per_target 2 \
+	--num_seq_per_target $seq_per_target \
 	--sampling_temp "0.1 0.2 0.3" \
 	--omit_AAs "XC"
-
-output_name=$(basename -- "$input_pdb")
-output_name="${output_name%.*}"
-
-# rename output fasta
-mv $output_dir/seqs/${output_name}.fa $output_dir/seqs/${name}.fa
 
 echo Done.
